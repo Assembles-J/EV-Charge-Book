1d5669512d16a59b747179d05ac9fed8fa1862bd
Peng <pengpeng_on@163.com>
2026-08-26 22:40:32 +0800

    fix(android): add dp import for vehicle catalog

--- android/app/src/main/java/com/evchargebook/ui/vehicle/VehicleCatalogScreen.kt
+++ android/app/src/main/java/com/evchargebook/ui/vehicle/VehicleCatalogScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.evchargebook.data.entity.VehicleCatalogEntity
 import com.evchargebook.ui.theme.spacing
 
