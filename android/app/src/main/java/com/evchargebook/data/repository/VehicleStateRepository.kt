03f1e6ee1b8caa6917fa1e03db2412c17ee029e1
Peng <pengpeng_on@163.com>
2026-08-27 22:23:51 +0800

    feat: add vehicle state repository foundation

---
+++ android/app/src/main/java/com/evchargebook/data/repository/VehicleStateRepository.kt
@@ -0,0 +1,17 @@
+package com.evchargebook.data.repository
+
+import com.evchargebook.data.dao.VehicleStateDao
+import com.evchargebook.data.entity.VehicleStateEntity
+import kotlinx.coroutines.flow.Flow
+
+class VehicleStateRepository(
+    private val dao: VehicleStateDao
+) {
+    fun observe(vehicleId: Long): Flow<VehicleStateEntity?> = dao.observe(vehicleId)
+
+    suspend fun get(vehicleId: Long): VehicleStateEntity? = dao.get(vehicleId)
+
+    suspend fun save(state: VehicleStateEntity) {
+        dao.upsert(state)
+    }
+}
