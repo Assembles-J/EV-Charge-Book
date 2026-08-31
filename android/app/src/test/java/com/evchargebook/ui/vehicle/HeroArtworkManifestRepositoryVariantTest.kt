d88caf2614e4f1a6a6fb8c421b46f8853bb0fa62
Peng <pengpeng_on@163.com>
2026-08-31 16:20:37 +0800

    test(android): align Hero variant tests with JUnit4

--- android/app/src/test/java/com/evchargebook/ui/vehicle/HeroArtworkManifestRepositoryVariantTest.kt
+++ android/app/src/test/java/com/evchargebook/ui/vehicle/HeroArtworkManifestRepositoryVariantTest.kt
@@ -1,7 +1,7 @@
 package com.evchargebook.ui.vehicle
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class HeroArtworkManifestRepositoryVariantTest {
     @Test
